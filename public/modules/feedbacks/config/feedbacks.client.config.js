4f697da6010c27e9c834a7d89296c33100ccb2aa
Andrei Constantinescu <andrei.c@icloud.com>
2014-05-18 15:27:27 +0300

    Some refactor. Restrict access to edit and create feedbacks.

--- public/modules/feedbacks/config/feedbacks.client.config.js
+++ public/modules/feedbacks/config/feedbacks.client.config.js
@@ -4,7 +4,6 @@
 angular.module('feedbacks').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('bottombar', 'Feedbacks', 'feedbacks');
 		Menus.addMenuItem('bottombar', 'Feedback Nou', 'feedbacks/create');
 	}
 ]);
