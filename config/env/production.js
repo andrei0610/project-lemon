04bc1635bb2addcf0911be7c0177b7f7cd7ee304
Andrei Constantinescu <andrei.c@icloud.com>
2014-06-04 00:30:22 +0300

    Change production db.

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://andrei.c@icloud.com:k86-rGc-Nt3-Cre@kahana.mongohq.com:10074/mds_staging'
-',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/project-lemon',
 	assets: {
 		lib: {
 			css: [
